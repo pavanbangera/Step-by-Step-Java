94dba7c4b0ed510455b5330a3f9f010d817cda57
Pavan Bangera <92009490+pavanbangera@users.noreply.github.com>
2023-07-08 22:04:11 +0530

    if else statement

--- Conditions.java
+++ Conditions.java
@@ -3,5 +3,13 @@ import java.util.*;
 public class Conditions {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Check whether given number is even or odd");
+        int a = sc.nextInt();
+        if (a % 2 == 0) {
+            System.out.println(a + " is even");
+
+        } else {
+            System.out.println(a + " is odd");
+        }
     }
 }
