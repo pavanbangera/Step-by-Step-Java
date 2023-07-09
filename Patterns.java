4041ae9cb3054751527123dcb901604db012015b
Pavan Bangera <92009490+pavanbangera@users.noreply.github.com>
2023-07-09 20:25:08 +0530

    half pyramid pattern

--- Patterns.java
+++ Patterns.java
@@ -27,8 +27,20 @@ public class Patterns {
         }
     }
 
+    public static void halfPyramid() {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         // solidRectangle();
-        hollowRectangle();
+        // hollowRectangle();
+        halfPyramid();
     }
 }
