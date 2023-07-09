db6415f4c3e89ecdb5af287335c70cc32253baa9
Pavan Bangera <92009490+pavanbangera@users.noreply.github.com>
2023-07-09 11:45:35 +0530

    while loop

--- Loops.java
+++ Loops.java
@@ -1,7 +1,13 @@
 public class Loops {
     public static void main(String[] args) {
-        for (int i = 0; i <= 10; i++) {
+        // for (int i = 0; i <= 10; i++) {
+        // System.out.println(i);
+        // }
+
+        int i = 0;
+        while (i <= 10) {
             System.out.println(i);
+            i++;
         }
     }
 }
