9277fd7459e7f1d7db43afcc46825c9bf52cf808
Pavan Bangera <92009490+pavanbangera@users.noreply.github.com>
2023-07-10 11:22:27 +0530

    butterfly pattern

--- Patterns.java
+++ Patterns.java
@@ -80,12 +80,41 @@ public class Patterns {
         }
     }
 
+    public static void butterflyPattern() {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= n + n - i - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         // solidRectangle();
         // hollowRectangle();
         // halfPyramid();
         // invertedHalfPyramid180();
         // fluidTraingle();
-        Traingle01();
+        // Traingle01();
+        butterflyPattern();
     }
 }
