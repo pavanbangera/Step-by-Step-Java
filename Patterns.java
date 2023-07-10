275e6e7dfbbfc5daab5e2cc2111e04537c130190
Pavan Bangera <92009490+pavanbangera@users.noreply.github.com>
2023-07-10 11:54:15 +0530

    palindromic pattern

--- Patterns.java
+++ Patterns.java
@@ -151,6 +151,22 @@ public class Patterns {
         }
     }
 
+    public static void palindromicPattern() {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         // solidRectangle();
         // hollowRectangle();
@@ -161,6 +177,7 @@ public class Patterns {
         // butterflyPattern();
         // solidRhombus();
         // solidRhombusWithHollowRect();
-        numbersPyramid();
+        // numbersPyramid();
+        palindromicPattern();
     }
 }
