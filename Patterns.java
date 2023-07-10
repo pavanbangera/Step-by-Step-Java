231612182a0dc08bc6b015895aba76cadda4ddae
Pavan Bangera <92009490+pavanbangera@users.noreply.github.com>
2023-07-10 11:01:34 +0530

    0-1 traingle pattern

--- Patterns.java
+++ Patterns.java
@@ -66,11 +66,26 @@ public class Patterns {
         }
     }
 
+    public static void Traingle01() {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         // solidRectangle();
         // hollowRectangle();
         // halfPyramid();
         // invertedHalfPyramid180();
-        fluidTraingle();
+        // fluidTraingle();
+        Traingle01();
     }
 }
