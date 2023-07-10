9c276e3b553baad3687b900fa054bf1604ee6864
Pavan Bangera <92009490+pavanbangera@users.noreply.github.com>
2023-07-10 11:34:44 +0530

    solidRhombusWithHollowRect pattern

--- Patterns.java
+++ Patterns.java
@@ -108,6 +108,36 @@ public class Patterns {
         }
     }
 
+    public static void solidRhombus() {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void solidRhombusWithHollowRect() {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || j == 1 || i == n || j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         // solidRectangle();
         // hollowRectangle();
@@ -115,6 +145,8 @@ public class Patterns {
         // invertedHalfPyramid180();
         // fluidTraingle();
         // Traingle01();
-        butterflyPattern();
+        // butterflyPattern();
+        // solidRhombus();
+        solidRhombusWithHollowRect();
     }
 }
