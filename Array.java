8a8c62a0bbc99d8576d889d788dd8eee1ad33081
Pavan Bangera <92009490+pavanbangera@users.noreply.github.com>
2023-07-11 18:25:39 +0530

    array

---
+++ Array.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Array {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array ");
+        int size = sc.nextInt();
+        int[] numbers = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            numbers[i] = sc.nextInt();
+        }
+        for (int i = 0; i < size; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println();
+        System.out.print("find a number from array ");
+        int f = sc.nextInt();
+        for (int i = 0; i < size; i++) {
+            if (f == numbers[i]) {
+                System.out.println(f + " found at the index of " + i);
+                break;
+            }
+        }
+    }
+}
