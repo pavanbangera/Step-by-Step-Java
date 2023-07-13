1c6ff86b4dbd570d3184271e88986b8f9b9c3bfe
Pavan Bangera <92009490+pavanbangera@users.noreply.github.com>
2023-07-13 12:07:25 +0530

    update bit manipulation

--- BitManipulation.java
+++ BitManipulation.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class BitManipulation {
 
     public static void getBit(int i, int j) {
@@ -31,9 +33,27 @@ public class BitManipulation {
 
     }
 
+    public static void updateBit(int i, int j) {
+        Scanner sc = new Scanner(System.in);
+        int opr = sc.nextInt();
+        int n = i;
+        int pos = j;
+        int bitMask = 1 << pos;
+        if (opr == 1) {
+            int newBit = bitMask | n;
+            System.out.println(newBit);
+        } else {
+            int newBitMask = ~(bitMask);
+            int newBit = newBitMask & n;
+            System.out.println(newBit);
+        }
+
+    }
+
     public static void main(String[] args) {
         // getBit(5, 2);
         // setBit(5, 1);
-        clearBit(5, 2);
+        // clearBit(5, 2);
+        updateBit(5, 1);
     }
 }
