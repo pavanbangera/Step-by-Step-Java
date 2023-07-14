c978652b6d5066a39594da0f31881b9355ee544a
Pavan Bangera <92009490+pavanbangera@users.noreply.github.com>
2023-07-14 15:54:28 +0530

    sum of n numbers using recursion

--- Recursions.java
+++ Recursions.java
@@ -1,5 +1,6 @@
 public class Recursions {
     public static void printNumber(int n) {
+
         if (n <= 0) {
             System.out.println("end of the Recursion");
             return;
@@ -8,8 +9,20 @@ public class Recursions {
         printNumber(n - 1);
     }
 
+    public static void printSum(int i, int n, int sum) {
+        if (i == n) {
+            sum += i;
+            System.out.println(sum);
+            return;
+        }
+        sum += i;
+        printSum(i + 1, n, sum);
+        System.out.println(i);
+    }
+
     public static void main(String[] args) {
-        int n = 6;
-        printNumber(n);
+        int n = 5;
+        // printNumber(n);
+        printSum(1, n, 0);
     }
 }
