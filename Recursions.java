5cfb4b7068efb35f22ebe261af85f04704798b05
Pavan Bangera <92009490+pavanbangera@users.noreply.github.com>
2023-07-23 11:38:21 +0530

    print keypad sub sequences of given string using recursion

--- Recursions.java
+++ Recursions.java
@@ -156,6 +156,21 @@ public class Recursions {
         subSequences(str, index + 1, newStr, set);
     }
 
+    public static String keypad[] = { "_,@", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+
+    public static void printCombi(String str, int index, String combination) {
+        if (index == str.length()) {
+            System.out.println(combination);
+            return;
+        }
+        char currentChar = str.charAt(index);
+        String mapping = keypad[currentChar - '0'];
+        for (int i = 0; i < mapping.length(); i++) {
+            printCombi(str, index + 1, combination + mapping.charAt(i));
+        }
+
+    }
+
     public static void main(String[] args) {
         // int n = 4;
         // printNumber(n);
@@ -181,7 +196,9 @@ public class Recursions {
         // moveAllX("axbxcxxdxx", 0, 'x');
 
         // removeDuplicates("abbcddde", 0);
-        subSequences("abc", 0, "", set);
+        // subSequences("abc", 0, "", set);
+
+        printCombi("26", 0, "");
 
     }
 }
