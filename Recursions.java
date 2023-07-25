2fdad0901371ffa2ca0f006a7a89c68e9368641a
Pavan Bangera <92009490+pavanbangera@users.noreply.github.com>
2023-07-25 16:52:06 +0530

    count total paths in a maza to move from (0,0) to (n.m) using recurtion

--- Recursions.java
+++ Recursions.java
@@ -184,6 +184,23 @@ public class Recursions {
         }
     }
 
+    public static int countPaths(int i, int j, int n, int m) {
+        if (i == n || j == m) {
+            return 0;
+        }
+
+        if (i == n - 1 && j == m - 1) {
+
+            return 1;
+        }
+
+        int countDown = countPaths(i, j + 1, n, m);
+
+        int countRight = countPaths(i + 1, j, n, m);
+
+        return countDown + countRight;
+    }
+
     public static void main(String[] args) {
         // int n = 4;
         // printNumber(n);
@@ -213,7 +230,9 @@ public class Recursions {
 
         // printCombi("26", 0, "");
 
-        printPermutation("abc", "");
+        // printPermutation("abc", "");
+
+        System.out.println(countPaths(0, 0, 3, 3));
 
     }
 }
