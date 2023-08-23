c28647428b9bcd4005bcf496a3e3e139020454be
Pavan Bangera <92009490+pavanbangera@users.noreply.github.com>
2023-08-23 11:39:12 +0530

    implement binary tree

---
+++ BinaryTree.java
@@ -0,0 +1,38 @@
+public class BinaryTree {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class CreateBT {
+        static int indx = -1;
+
+        public static Node buildBT(int nodes[]) {
+            indx++;
+            if (nodes[indx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[indx]);
+            newNode.left = buildBT(nodes);
+            newNode.right = buildBT(nodes);
+
+            return newNode;
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+
+        CreateBT tree = new CreateBT();
+        Node root = tree.buildBT(nodes);
+        System.out.println(root.data);
+    }
+}
